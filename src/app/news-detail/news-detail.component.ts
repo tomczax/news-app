07060d10ba960f4ef24d0588e85eed929ca04a20
Karolina <tomczaxon@gmail.com>
2021-01-24 17:47:02 +0100

    News detail done

--- src/app/news-detail/news-detail.component.ts
+++ src/app/news-detail/news-detail.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
-import { Article } from 'angular-news-api';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Article } from "angular-news-api";
+import { Observable } from "rxjs";
+import { map, tap } from "rxjs/operators";
 
-import { ArticleComment } from '../models/comment.model';
+import { ArticleComment } from "../models/comment.model";
 
 @Component({
-    selector: 'app-news-detail',
-    templateUrl: './news-detail.component.html',
-    styleUrls: ['./news-detail.component.scss']
+    selector: "app-news-detail",
+    templateUrl: "./news-detail.component.html",
+    styleUrls: ["./news-detail.component.scss"],
 })
 export class NewsDetailComponent implements OnInit {
     public article$: Observable<Article>;
 
     public comments: ArticleComment[] = [
         {
-            name: 'Chris Nat',
+            name: "Chris Nat",
             date: new Date(),
-            avatar: '/assets/images/1.jpg',
+            avatar: "/assets/images/1.jpg",
             comment: `Lorem ipsum dolor sit amet, consectetur
                       adipiscing elit, sed do eiusmod tempor
                       incididunt ut labore et dolore magna aliqua.
@@ -27,9 +27,9 @@ export class NewsDetailComponent implements OnInit {
                       ullamco laboris nisi ut aliquip ex ea commodo consequat`,
         },
         {
-            name: 'Matt Damon',
+            name: "Matt Damon",
             date: new Date(),
-            avatar: '/assets/images/2.jpg',
+            avatar: "/assets/images/2.jpg",
             comment: `Ut enim ad minim veniam, quis nostrud exercitation
                       ullamco laboris nisi ut aliquip ex ea commodo consequat.
                       Lorem ipsum dolor sit amet, consectetur
@@ -37,9 +37,9 @@ export class NewsDetailComponent implements OnInit {
                       incididunt ut labore et dolore magna aliqua. `,
         },
         {
-            name: 'Seb Willhelm',
+            name: "Seb Willhelm",
             date: new Date(),
-            avatar: '/assets/images/3.jpg',
+            avatar: "/assets/images/3.jpg",
             comment: `Duis aute irure dolor in reprehenderit in voluptate
                   velit esse cillum dolore eu fugiat nulla pariatur.
                   Excepteur sint occaecat cupidatat non proident, sunt
@@ -48,22 +48,31 @@ export class NewsDetailComponent implements OnInit {
     ];
 
     constructor(
-        public activatedRoute: ActivatedRoute
-    ) { }
+        public activatedRoute: ActivatedRoute,
+        private router: Router
+    ) {}
+
+    get commentsTitle(): string {
+        const commentsCount = this.comments.length;
+        return commentsCount === 1 ? "1 Comment" : `${commentsCount} Comments`;
+    }
 
     public ngOnInit() {
         this.article$ = this.activatedRoute.paramMap.pipe(
-            map(() => window.history.state)
+            map(() =>
+                window.history.state.navigationId === 1
+                    ? null
+                    : window.history.state
+            ),
+            tap(
+                (state: any) =>
+                    state === null && this.router.navigate(["/news"])
+            )
         );
-
-        // Make sure that this page can only be
-        // accessible from the main News list with
-        // a loaded article from 'state', if url entered
-        // manually from the browser it should go back to
-        // the /news view
     }
 
+    // Open original article in new tab
     public goToArticle(url: string): void {
-        // Open original article in new tab
+        window.open(url, "_blank");
     }
 }
