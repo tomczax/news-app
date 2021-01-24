76c3d29d25b4e680dc1d0401edf2a54b2c7c7de6
Karolina <tomczaxon@gmail.com>
2021-01-24 19:17:53 +0100

    Change api key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import {
     MatToolbarModule, MatIconModule, MatMenuModule,
     MatButtonModule, MatFormFieldModule, MatInputModule,
-    MatProgressSpinnerModule, MatProgressBarModule
+    MatProgressSpinnerModule, MatProgressBarModule,
+    MatSnackBarModule
 } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,7 +18,8 @@ import { NewsDetailComponent } from './news-detail/news-detail.component';
 import { NewsComponent } from './news/news.component';
 
 const newsApiConfig: NewsApiKeyConfig = {
-    key: 'f147c4c717d34dfd822ed53665d1ba09'
+    // key: 'f147c4c717d34dfd822ed53665d1ba09'
+    key: 'e67018ffe6df4eaa9098b3e0916f8a7c'
   };
 
 @NgModule({
@@ -39,6 +41,7 @@ const newsApiConfig: NewsApiKeyConfig = {
         FormsModule,
         MatProgressSpinnerModule,
         MatProgressBarModule,
+        MatSnackBarModule,
         NgnewsModule.forRoot(newsApiConfig)
     ],
     providers: [],
